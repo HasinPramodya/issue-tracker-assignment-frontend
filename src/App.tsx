aa641ccee59ca298823138d3df609a3eaf246f9d
hasin.pramodya <pramodyahasin@gmail.com>
2026-02-10 14:11:29 +0530

    Implement initial frontend application with authentication, routing, and core issue management features.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthProvider } from './components/context/AuthContext';
+import Routing from './components/Routing';
 
 function App() {
-
-
   return (
-    <>
-
-    </>
-  )
+    <Router>
+      <AuthProvider>
+        <Routing />
+      </AuthProvider>
+    </Router>
+  );
 }
 
-export default App
+export default App;
