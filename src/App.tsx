5ff3a5c17d0b1ab69d106085e4a91f92f1d48dfe
hasin.pramodya <pramodyahasin@gmail.com>
2026-02-12 18:21:02 +0530

    remove unsused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './components/context/AuthContext';
 import Routing from './components/Routing';
